1946383f499f1fc44df4956eaa4ae6c1d9bdfa09
hoorasadat <mirsaanei@gmail.com>
2023-08-18 19:26:42 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbFstNm = new System.Windows.Forms.TextBox();
+            this.txtbLstNm = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.txtbPwrUsg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::Lab_2.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(158, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.label1.Size = new System.Drawing.Size(390, 129);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bill app";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Window;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(18, 43);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.label2.Size = new System.Drawing.Size(114, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(18, 97);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.label3.Size = new System.Drawing.Size(112, 39);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name:";
+            // 
+            // txtbFstNm
+            // 
+            this.txtbFstNm.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbFstNm.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.txtbFstNm.Location = new System.Drawing.Point(183, 56);
+            this.txtbFstNm.Name = "txtbFstNm";
+            this.txtbFstNm.Size = new System.Drawing.Size(156, 29);
+            this.txtbFstNm.TabIndex = 4;
+            // 
+            // txtbLstNm
+            // 
+            this.txtbLstNm.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbLstNm.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.txtbLstNm.Location = new System.Drawing.Point(183, 110);
+            this.txtbLstNm.Name = "txtbLstNm";
+            this.txtbLstNm.Size = new System.Drawing.Size(156, 29);
+            this.txtbLstNm.TabIndex = 5;
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.SystemColors.Info;
+            this.addBtn.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.addBtn.Location = new System.Drawing.Point(428, 97);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.addBtn.Size = new System.Drawing.Size(148, 45);
+            this.addBtn.TabIndex = 6;
+            this.addBtn.Text = "Add Customer";
+            this.addBtn.UseVisualStyleBackColor = false;
+            // 
+            // txtbPwrUsg
+            // 
+            this.txtbPwrUsg.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbPwrUsg.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.txtbPwrUsg.Location = new System.Drawing.Point(183, 164);
+            this.txtbPwrUsg.Name = "txtbPwrUsg";
+            this.txtbPwrUsg.Size = new System.Drawing.Size(156, 29);
+            this.txtbPwrUsg.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(18, 151);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.label4.Size = new System.Drawing.Size(156, 39);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Customer Usage:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.addBtn);
+            this.groupBox1.Controls.Add(this.txtbPwrUsg);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtbFstNm);
+            this.groupBox1.Controls.Add(this.txtbLstNm);
+            this.groupBox1.Location = new System.Drawing.Point(-5, 149);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(618, 217);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add a new Customer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = global::Lab_2.Properties.Resources.blurBG;
+            this.ClientSize = new System.Drawing.Size(612, 627);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Info;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbFstNm;
+        private System.Windows.Forms.TextBox txtbLstNm;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.TextBox txtbPwrUsg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
